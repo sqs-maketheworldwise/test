235dddf3869a333b70d12bb7866b879767bfc348
Ahn Seung Seop <maketheworldwise@squares.ai>
2024-05-31 12:00:36 +0900

    test

---
+++ test.ts
@@ -0,0 +1,43 @@
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
+function test1() {
+    console.log('test1');
+}
+
+// TODO test2
+// BODY: test2
+function test2() {
+    console.log('test2');
+}
+
+// todo: test3
+// body: test3
+function test3() {
+    console.log('test3');
+}
+
+/**
+ * todo test4
+ * body test4
+ */
+function test4() {
+    console.log('test4');
+}
+
+/**
+ * todo: test5
+ * body: test5
+ */
+function test5() {
+    console.log('test5');
+}
+
+/**
+ * TODO: test6
+ * BODY: test6
+ */
+function test6() {
+    console.log('test6');
+}
